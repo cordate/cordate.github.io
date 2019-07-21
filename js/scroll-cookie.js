c026af2266d95e55ff0f0c37cec208fd02bd11c9
Gavin Zhang <gavinzhang@GavindeMacBook-Pro.local>
2019-07-22 01:03:29 +0800

    Site updated: 2019-07-22 01:03:28

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 22 2019 00:42:42 GMT+0800 (CST)
+// build time:Mon Jul 22 2019 01:02:26 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
